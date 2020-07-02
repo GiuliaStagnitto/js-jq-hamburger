19c66c53e4e432d829152a193a202ae719724aa8
Giux <giuliamelody@gmail.com>
2020-07-02 18:04:17 +0200

    .1

---
+++ main.js
@@ -0,0 +1,8 @@
+// Al click di header right devo mostrare hamburger menu
+
+// creo var con media query
+va
+$('header-right  i').click(function() {
+  $('hamburger-menu').addClass('hamburger-menu.active');
+  console.log('hamburger-menu');
+});
